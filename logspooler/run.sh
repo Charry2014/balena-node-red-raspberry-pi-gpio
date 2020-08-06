fba21dfc5528d00b8de22ae063ee2dd4f2d20ce9
Charry2014 <58067238+Charry2014@users.noreply.github.com>
2020-08-06 13:58:49 +0200

    Remove dead code

--- logspooler/run.sh
+++ logspooler/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#rm -rf /data/logs/node.log
-#touch /data/logs/node.log
-#curl -X POST -H "Content-Type: application/json" --data '{"follow":true,"all":true}' "$BALENA_SUPERVISOR_ADDRESS/v2/journal-logs?apikey=$BALENA_SUPERVISOR_API_KEY" > /data/logs/node.log
-
-
-
 LOG_FILE=/data/logs/node.log
 
 # This is some nasty hacky log rotation - simply remove the logs file every time the container restarts
