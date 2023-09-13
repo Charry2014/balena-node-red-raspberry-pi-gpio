e2bcf3d3f1c74992558088563b6c72d11d2959a6
Charry2014 <58067238+Charry2014@users.noreply.github.com>
2023-09-13 09:01:00 +0200

    2023 update

--- node-red-pigpiod/startup.sh
+++ node-red-pigpiod/startup.sh
@@ -4,8 +4,17 @@
 rm -f /var/run/pigpio.pid
 /usr/local/bin/pigpiod -g
 
-/usr/local/bin/pigs w 5  0
-/usr/local/bin/pigs w 6  0
-/usr/local/bin/pigs w 13 0
-/usr/local/bin/pigs w 16 0
-/usr/local/bin/pigs w 19 0
+# Set the outputs to 1 - which represents also the power off state of the relays
+# 5 Channels of water
+/usr/local/bin/pigs w 5  1
+/usr/local/bin/pigs w 6  1
+/usr/local/bin/pigs w 13 1
+/usr/local/bin/pigs w 16 1
+/usr/local/bin/pigs w 19 1
+# Gate
+/usr/local/bin/pigs w 20 1
+
+# Set the channels to inputs and set pullup/pulldown resistor
+# Garage - exit release on gate
+/usr/local/bin/pigs m 21 r
+/usr/local/bin/pigs pud 21 u 
